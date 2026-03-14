a66e0d722dc4db07466bdc3a836f7fd74bae685e
FMR04 <franciscomedinaromero5@gmail.com>
2026-03-14 13:21:56 +0100

    Refactorizacion

--- Cuadrado.java
+++ Cuadrado.java
@@ -1,7 +1,9 @@
-package figuras;
+package Figuras.src.main.java.figuras;
+
 import java.awt.Color;
-public class Cuadrado extends Rectángulo{
-public Cuadrado (double x, double y, Color color, double lado){
-super (x, y, color, lado, lado);
-}
+
+public class Cuadrado extends Rectangulo {
+	public Cuadrado(double x, double y, Color color, double lado) {
+		super(x, y, color, lado, lado);
+	}
 }
