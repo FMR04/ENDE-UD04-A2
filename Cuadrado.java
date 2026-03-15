f488dcc663ae3a3893d9132883e33cdf7443d1ee
FMR04 <franciscomedinaromero5@gmail.com>
2026-03-15 12:36:37 +0100

    Añadido un Sys.out para mostrar el lado cuadrado

--- Cuadrado.java
+++ Cuadrado.java
@@ -5,5 +5,6 @@ import java.awt.Color;
 public class Cuadrado extends Rectangulo {
 	public Cuadrado(double x, double y, Color color, double lado) {
 		super(x, y, color, lado, lado);
+		System.out.println("Cuadrado creado con lado: " + lado);
 	}
 }
